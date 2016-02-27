44dcc21469e4000d68781112236cbdcb518c2041
Aditya Singh Chauhan <astroadityachauhan@gmail.com>
2016-02-27 10:31:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-
+<html>
+<form method="post" action="calculate_three_nearest_coordinates.php">
+<h3>Enter x-coordinate :
+<input type="text" name="x" id="x" />
+<br/>
+<h3>Enter y-coordinate :
+<input type="text" name="y" id="y" />
+<br/>
+<input type="submit" value="submit"/>
+</h3>
+</form>
+</html>
